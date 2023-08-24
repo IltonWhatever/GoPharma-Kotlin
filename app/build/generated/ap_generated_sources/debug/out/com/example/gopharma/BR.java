21d0ea582c6d8ab95cba36778775aea78d1bd943
Ilton Whatever <ashpalete18@gmail.com>
2023-08-24 17:02:47 -0300

    Reploud do Projeto

---
+++ app/build/generated/ap_generated_sources/debug/out/com/example/gopharma/BR.java
@@ -0,0 +1,5 @@
+package com.example.gopharma;
+
+public class BR {
+  public static final int _all = 0;
+}
---
+++ app/src/main/java/com/example/gopharma/view/PropostasActivity.kt
@@ -0,0 +1,70 @@
+package com.example.gopharma.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Adapter
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import com.example.gopharma.R
+import com.example.gopharma.databinding.ActivityPropostasBinding
+import com.example.gopharma.models.Cliente
+import com.example.gopharma.util.Endpoints
+import com.example.gopharma.util.ServerHTTP
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class PropostasActivity : AppCompatActivity(), View.OnClickListener{
+    lateinit var binding : ActivityPropostasBinding
+    lateinit var clientes : ArrayList<Cliente>
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityPropostasBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textView.setOnClickListener(this)
+
+        binding.listPropostas.setOnItemClickListener{parent, view, position, id->
+            binding.boxbox.setText(clientes.get(position).nome)
+        }
+
+        listCliente()
+    }
+
+    override fun onClick(v:View) {
+
+    }
+
+    fun listCliente(){
+        val retrofitClient = ServerHTTP
+            .getRetrofitInstance("http://192.168.0.65/")
+
+        val endpoint = retrofitClient.create(Endpoints::class.java)
+
+        val callback = endpoint.getCliente()
+
+        // on below line we are executing our method.
+        callback.enqueue(object : Callback<List<Cliente>> {
+            override fun onResponse(call: Call<List<Cliente>>, response: Response<List<Cliente>>) {
+                // this method is called when we get response from our api.
+                //Toast.makeText(this@CadastroActivity, "Cliente Cadastrado com Sucesso", Toast.LENGTH_SHORT).show()
+                clientes = ArrayList<Cliente>()
+                response.body()?.forEach { clientes.add(it) }
+
+                val adapter = ArrayAdapter(baseContext, android.R.layout.simple_list_item_1, clientes)
+                binding.listPropostas.adapter = adapter
+
+            }
+
+
+            override fun onFailure(call: Call<List<Cliente>>, t: Throwable) {
+                // setting text to our text view when
+                // we get error response from API.
+                Toast.makeText(baseContext, t.message, Toast.LENGTH_SHORT).show()
+                // responseTV.setText("Error found is : " + t.message)
+            }
+        })
+    }
+}
